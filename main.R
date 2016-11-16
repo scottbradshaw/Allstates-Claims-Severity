2347b9b3fe957d0417f60a174b27d4944e405a32
scottbradshaw <scottbrads@gmail.com>
2016-11-16 09:40:16 +0000

    First commit

--- main.R
+++ main.R
@@ -1,7 +1,1 @@
-library(data.table)
-packrat::library(ranger,lib.loc="/r_packages")
-install.packages("ranger")
-packrat::install_local("stringr")
-
-install.packages("ranger",lib="/r_packages")
-
+# Initial commit
